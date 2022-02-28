0a2cdd58f324e9e9db27b96f4022bb69dd340597
Balbader <15971200+Balbader@users.noreply.github.com>
2022-02-28 14:07:26 +0100

    add monsters array in CardList component

--- src/App.js
+++ src/App.js
@@ -20,10 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList />
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList name='Basil'>
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+          }
+        </CardList>
       </div>
     );
   }

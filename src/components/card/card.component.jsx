bd1d1fdfb49b79dd4fc7cd283d6fb1e048b4a4a4
Balbader <15971200+Balbader@users.noreply.github.com>
2022-02-28 14:44:09 +0100

    add img && email display to Card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './card.styles.css';
 
-export const Card = (props) => {
+export const Card = props => {
   return (
-    <div>
-      <h1> {props.monster.name} </h1>
+    <div className='card-container'>
+      <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2`} />
+      <h2> {props.monster.name} </h2>
+      <p> {props.monster.email} </p>
     </div>
   )
 }

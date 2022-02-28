d472d6fef10efa96261e3e548bc462dfe7531330
Balbader <15971200+Balbader@users.noreply.github.com>
2022-02-28 13:21:57 +0100

    update App component to display arr of monsters name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {                                       // wait for the component to mount
+    fetch('https://jsonplaceholder.typicode.com/users')       // fetch our users from the json file
+      .then(response => response.json())                      // .json() will give us back the response in a json format 
+      .then(users => this.setState({ monsters: users }))      // setting the monsters to this array of users
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList />
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
